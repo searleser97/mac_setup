e27cc51eec25dfef3d779d463c7b9c0e6ffdf317
Sergio <gabrielsanv97@gmail.com>
2024-04-29 15:52:09 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/sergiosanc/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
+brew install wget
+mkdir ~/ProgramFiles
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz
+tar -xvzf ~/ProgramFiles/nvim-linux64.tar.gz -C ~/ProgramFiles/
+git clone https://github.com/searleser97/nvim_lua ~/.config/nvim
