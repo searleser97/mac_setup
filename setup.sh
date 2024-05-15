dd01cfef39e777390474784de248f8344aae8bf1
Sergio <gabrielsanv97@gmail.com>
2024-05-15 10:32:17 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ mkdir ~/ProgramFiles
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz
 tar -xvzf ~/ProgramFiles/nvim-linux64.tar.gz -C ~/ProgramFiles/
 git clone https://github.com/searleser97/nvim_lua ~/.config/nvim
+
+brew install --cask visual-studio-code
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
