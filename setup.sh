17d03c89e94122ad687decf15c08e01743fe5bd8
Sergio <gabrielsanv97@gmail.com>
2024-05-21 10:36:38 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,17 +2,31 @@
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/sergiosanc/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install wget
+brew install ripgrep
 # install nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# install python
+brew install pyenv
+
+brew install git-delta
+brew install --cask iterm2
+brew install --cask displaylink
+brew install --cask android-studio
+brew install --cask visual-studio-code
+brew install --cask nordvpn
 
 mkdir ~/ProgramFiles
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz
 tar -xvzf ~/ProgramFiles/nvim-linux64.tar.gz -C ~/ProgramFiles/
 git clone https://github.com/searleser97/nvim_lua ~/.config/nvim
 
-brew install --cask iterm2
-brew install --cask displaylink
-brew install --cask android-studio
-brew install --cask visual-studio-code
+# save git credentials in computer
+git config --global credential.helper store
+# set nvim as default git editor
+git config --global core.editor "nvim"
+# auto create branches on remote locally
+git config --global push.autoSetupRemote true
+# set delta as default pager
+git config --global core.pager "delta"
