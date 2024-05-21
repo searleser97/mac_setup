e8677118f43866718f318cd97e66fb8427992c94
Sergio <gabrielsanv97@gmail.com>
2024-05-21 10:25:29 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,17 @@
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/sergiosanc/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install wget
+# install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 mkdir ~/ProgramFiles
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz
 tar -xvzf ~/ProgramFiles/nvim-linux64.tar.gz -C ~/ProgramFiles/
 git clone https://github.com/searleser97/nvim_lua ~/.config/nvim
 
+brew install --cask iterm2
+brew install --cask displaylink
+brew install --cask android-studio
 brew install --cask visual-studio-code
-
-# install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
